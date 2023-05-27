0e031e1a96f22b0a8280e773fca301f7c74d54cc
irfanhfzh <irfanhafizh53@gmail.com>
2023-05-27 17:48:04 +0700

    updated commits

---
+++ src/components/NewTweetForm.tsx
@@ -0,0 +1,104 @@
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import {
+  useCallback,
+  useLayoutEffect,
+  useRef,
+  useState,
+  type FormEvent,
+} from "react";
+import { api } from "~/utils/api";
+import { Button } from "./Button";
+import { ProfileImage } from "./ProfileImage";
+
+function updateTextAreaSize(texetArea?: HTMLTextAreaElement) {
+  if (texetArea == null) return;
+  texetArea.style.height = "0";
+  texetArea.style.height = `${texetArea.scrollHeight}px`;
+}
+
+export function NewTweetForm() {
+  const session = useSession();
+  if (session.status !== "authenticated") return null;
+
+  return <Form />;
+}
+
+function Form() {
+  const session = useSession();
+  const [inputValue, setInputValue] = useState("");
+  const textAreaRef = useRef<HTMLTextAreaElement>();
+  const inputRef = useCallback((textArea: HTMLTextAreaElement) => {
+    updateTextAreaSize(textArea);
+    textAreaRef.current = textArea;
+  }, []);
+  const trpcUtils = api.useContext();
+
+  useLayoutEffect(() => {
+    updateTextAreaSize(textAreaRef.current);
+  }, [inputValue]);
+
+  const createTweet = api.tweet.create.useMutation({
+    onSuccess: (newTweet) => {
+      setInputValue("");
+
+      if (session.status != "authenticated") return;
+
+      trpcUtils.tweet.infiniteFeed.setInfiniteData({}, (oldData) => {
+        if (oldData == null || oldData.pages[0] == null) return;
+
+        const newCacheTweet = {
+          ...newTweet,
+          likeCount: 0,
+          likedByMe: false,
+          user: {
+            id: session.data?.user.id,
+            name: session.data?.user.name || null,
+            image: session.data?.user.image || null,
+          },
+        };
+
+        return {
+          ...oldData,
+          pages: [
+            {
+              ...oldData.pages[0],
+              tweets: [newCacheTweet, ...oldData.pages[0].tweets],
+            },
+            ...oldData.pages.slice(1),
+          ],
+        };
+      });
+    },
+  });
+
+  if (session.status !== "authenticated") return null;
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    createTweet.mutate({ content: inputValue });
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-2 border-b px-4 py-2"
+    >
+      <div className="flex gap-4">
+        <Link href={`/profiles/${session.data.user.id}`}>
+          <ProfileImage src={session.data.user.image} />
+        </Link>
+        <textarea
+          ref={inputRef}
+          placeholder="What's happening ?"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          style={{ height: 0 }}
+          className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
+        />
+      </div>
+      <Button className="self-end">Tweet</Button>
+    </form>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
-import { api } from "~/utils/api";
+import Head from "next/head";
+import { SideNav } from "~/components/SideNav";
 import "~/styles/globals.css";
+import { api } from "~/utils/api";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -10,7 +12,17 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Head>
+        <title>T3 Stack Twitter Clone</title>
+        <meta name="description" content="Twitter Clone build using T3 Stack" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="container mx-auto flex items-start sm:pr-4">
+        <SideNav />
+        <div className="min-h-screen flex-grow border-x">
+          <Component {...pageProps} />
+        </div>
+      </div>
     </SessionProvider>
   );
 };
